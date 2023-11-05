ac878d6b162ff5ef691b4c1ad8883709a6d5bc1d
tarin <tarinakter567@gmail.com>
2023-11-05 21:54:47 +0600

    Initial commit

---
+++ lib/pages/homePage.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+
+class Hompage extends StatelessWidget {
+  const Hompage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      
+
+      bottomNavigationBar: BottomNavigationBar(
+
+        items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+        BottomNavigationBarItem(icon: Icon(Icons.density_small_sharp), label: "Transection"),
+        BottomNavigationBarItem(icon: Icon(Icons.family_restroom_rounded), label: "People"),
+        //BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+      ],
+
+      ),
+
+    );
+  }
+}
+
