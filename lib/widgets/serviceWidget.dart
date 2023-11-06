a67e9371c3ad8a6f0ce7f93b86535617515f5992
tarin <tarinakter567@gmail.com>
2023-11-06 20:26:27 +0600

    create serviceWidget and service class

---
+++ lib/widgets/serviceWidget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:nagad/datas/listViewItems.dart';
+
+class ServiceWidget extends StatelessWidget {
+  const ServiceWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+
+    );
+  }
+}
