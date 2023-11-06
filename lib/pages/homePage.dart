25a546c7ceebb5c293c09e2dae2bf89a80931828
tarin <tarinakter567@gmail.com>
2023-11-06 10:18:54 +0600

    add image in assets file and add container for text in homepage.

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -14,7 +14,7 @@ class Homepage extends StatelessWidget {
 
         items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.density_small_sharp), label: "Transection"),
+        BottomNavigationBarItem(icon: Icon(Icons.density_small_sharp), label: "Transaction"),
         BottomNavigationBarItem(icon: Icon(Icons.family_restroom_rounded), label: "People"),
         //BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
       ],
@@ -22,8 +22,25 @@ class Homepage extends StatelessWidget {
       ),
 
       body: Container(
+        width: 460,
+        height: 168,
 
-      )
+        child: const Center(
+          child:  Column(
+            children: [
+              Text("নগদ",style: TextStyle(
+                fontSize:70,
+                fontWeight: FontWeight.w600,
+                //color: Colors.white
+              ),),
+              Text("ডাক বিভাগের ডিজিটাল লেনদেন",style: TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.w400
+              ),)
+            ],
+          ),
+        ),
+      ),
 
     );
   }
