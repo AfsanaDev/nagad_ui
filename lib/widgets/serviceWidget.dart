e4f6084a55091d0b865140d032617209d31347f7
tarin <tarinakter567@gmail.com>
2023-11-08 10:49:57 +0600

    change icons path and set these path in serviceWidget.

--- lib/widgets/serviceWidget.dart
+++ lib/widgets/serviceWidget.dart
@@ -2,30 +2,72 @@ import 'package:flutter/material.dart';
 import 'package:nagad/datas/listViewItems.dart';
 
 class ServiceWidget extends StatelessWidget {
-  List listItemName=[
-
-    ListViewItem(gridViewItems: " assets/send_money.png", textItems: " Send Money"),
+  List listItemName = [
+    ListViewItem(
+        gridViewItems: " assets/send_money.png", textItems: " Send Money"),
     ListViewItem(gridViewItems: " assets/cash_out.png", textItems: " Cash Out"),
-    ListViewItem(gridViewItems: " assets/mobile_recharge.png", textItems: " Mobile Recharge"),
-    ListViewItem(gridViewItems: " assets/add_money.png", textItems: " Add Money"),
-    ListViewItem(gridViewItems: " assets/transfer_money.png", textItems: " Transfer Money"),
-    ListViewItem(gridViewItems: " assets/insurance.png", textItems: " Insurance"),
-    ListViewItem(gridViewItems: " assets/nagad_mela.png", textItems: " Nagad Mela"),
+    ListViewItem(
+        gridViewItems: " assets/mobile_recharge.png",
+        textItems: " Mobile Recharge"),
+    ListViewItem(
+        gridViewItems: " assets/icons/add_money.png", textItems: " Add Money"),
+    ListViewItem(
+        gridViewItems: " assets/icons/transfer_money.png",
+        textItems: " Transfer Money"),
+    ListViewItem(
+        gridViewItems: " assets/icons/insurance.png", textItems: " Insurance"),
+    ListViewItem(
+        gridViewItems: " assets/icons/nagad_mela.png",
+        textItems: " Nagad Mela"),
   ];
 
   ServiceWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
-        Text("Services",style: TextStyle(
-        color: Color(0xFF6E6E6E),
-        fontSize: 16,
-        fontFamily: 'Inter',
-        fontWeight: FontWeight.w600,
-        height: 0,
-        ),)
+        const Align(
+          alignment: Alignment.topLeft,
+          child: Padding(
+            padding: EdgeInsets.only(left: 30, top: 35),
+            child: Text(
+              "Services",
+              style: TextStyle(
+                color: Color(0xFF6E6E6E),
+                fontSize: 16,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w600,
+                height: 0,
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          width: double.infinity,
+          height: 240,
+          child: GridView.builder(
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.red,
+                    ),
+                    child: Image(
+                        height: 80,
+                        image: AssetImage(listItemName[index].gridViewItems)),
+                  ),
+                  Text(listItemName[index].textItems)
+                ],
+              );
+            },
+            itemCount: listItemName.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4, mainAxisSpacing: 1, crossAxisSpacing: 1),
+          ),
+        ),
       ],
     );
   }
